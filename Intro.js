4161f03bd7a254096b3629a382e16ac693c857c8
ljlclark <35316538+ljlclark@users.noreply.github.com>
2022-09-30 07:46:30 +0800

    Update Pages

--- Intro.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// Intro.js
-
-function GoToInternalScript()
-{
-  window.location.href = "6InternalScript.html";
-}
---
+++ IntroToJS.js
@@ -0,0 +1,48 @@
+"use strict";
+// IntroToJS.js
+
+function GoToInternalScript()
+{
+  //window.location.href = "6InternalScript.html";
+  location.href = "6InternalScript.html";
+}
+
+function ShowWindowValues()
+{
+  //let elem = document.getElementById("windowValues");
+  //let elem = window.windowValues;
+  let elem = windowValues;
+  //windows.elem.innerHTML = "Window Values<br />";
+  elem.innerHTML = "Window Values<br />";
+  elem.innerHTML += `document...<br />`;
+  elem.innerHTML += `location...<br />`;
+  elem.innerHTML += `&nbsp;&nbsp;location.href: ${location.href} <br />`;
+}
+
+function ShowDocumentValues()
+{
+  //let elem = document.getElementById("documentValues");
+  //let elem = window.documentValues;
+  let elem = documentValues;
+
+  elem.innerHTML = "Document Values<br />"; 
+  //elem.innerHTML += `nodeType: ${window.document.nodeType}<br />`;
+  elem.innerHTML += `nodeType: ${document.nodeType}(Document)<br />`;
+  elem.innerHTML += `nodeName: ${document.nodeName}<br />`
+  elem.innerHTML += `documentElement(html)...<br />`;
+  elem.innerHTML += `head...<br />`;
+  elem.innerHTML += `body...<br />`;
+}
+
+function ShowParagraphValues()
+{
+  //let elem = document.getElementById("paragraphValues");
+  //let elem = window.paragraphValues;
+  let elem = paragraphValues;
+  elem.innerHTML = "Paragraph Values<br />"; 
+  elem.innerHTML += `nodeType: ${elem.nodeType}(Element)<br />`;
+  elem.innerHTML += `tagName: ${elem.tagName}<br />`
+  elem.innerHTML += `localName: ${elem.localName}<br />`
+  elem.innerHTML += `id: ${elem.id}<br />`
+  elem.innerHTML += `className: ${elem.className}<br />`
+}
