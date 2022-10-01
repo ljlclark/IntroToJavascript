7ab71c2d6e0f2a443e335f8a7b6914253bf5d84e
ljlclark <35316538+ljlclark@users.noreply.github.com>
2022-10-02 05:38:55 +0800

    Update Pages

--- IntroToJS.js
+++ IntroToJS.js
@@ -1,6 +1,7 @@
 "use strict";
 // IntroToJS.js
 
+// The 7ExternalScript.html - Button click event handler.
 function GoToInternalScript()
 {
   //window.location.href = "6InternalScript.html";
@@ -14,8 +15,8 @@ function ShowWindowValues()
   let elem = windowValues;
   //windows.elem.innerHTML = "Window Values<br />";
   elem.innerHTML = "Window Values<br />";
-  elem.innerHTML += `document...<br />`;
-  elem.innerHTML += `location...<br />`;
+  elem.innerHTML += "document...<br />";
+  elem.innerHTML += "location...<br />";
   elem.innerHTML += `&nbsp;&nbsp;location.href: ${location.href} <br />`;
 }
 
@@ -25,13 +26,13 @@ function ShowDocumentValues()
   //let elem = window.documentValues;
   let elem = documentValues;
 
-  elem.innerHTML = "Document Values<br />"; 
+  elem.innerHTML = "Document Values<br />";
   //elem.innerHTML += `nodeType: ${window.document.nodeType}<br />`;
   elem.innerHTML += `nodeType: ${document.nodeType}(Document)<br />`;
   elem.innerHTML += `nodeName: ${document.nodeName}<br />`
-  elem.innerHTML += `documentElement(html)...<br />`;
-  elem.innerHTML += `head...<br />`;
-  elem.innerHTML += `body...<br />`;
+  elem.innerHTML += "documentElement(html)...<br />";
+  elem.innerHTML += "head...<br />";
+  elem.innerHTML += "body...<br />";
 }
 
 function ShowParagraphValues()
@@ -39,10 +40,31 @@ function ShowParagraphValues()
   //let elem = document.getElementById("paragraphValues");
   //let elem = window.paragraphValues;
   let elem = paragraphValues;
-  elem.innerHTML = "Paragraph Values<br />"; 
+  elem.innerHTML = "Paragraph Values<br />";
   elem.innerHTML += `nodeType: ${elem.nodeType}(Element)<br />`;
   elem.innerHTML += `tagName: ${elem.tagName}<br />`
   elem.innerHTML += `localName: ${elem.localName}<br />`
   elem.innerHTML += `id: ${elem.id}<br />`
   elem.innerHTML += `className: ${elem.className}<br />`
+}
+
+class TitleClass
+{
+  // Initializes an object instance.
+  constructor()
+  {
+  }
+
+  // Adds the required event handler assignment.
+  AddEvents()
+  {
+    //let pageTitle = document.getElementById("pageTitle");
+    pageTitle.addEventListener("click", this.TitleClick.bind(this));
+  }
+
+  // The title click event handler.
+  TitleClick()
+  {
+    alert("The Title was clicked.");
+  }
 }
